017f9656997ad286fc389ac14d023a2c55cde396
davidesantopietro <davide.santopietro@itsvil.it>
2023-03-20 12:46:29 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,37 +31,39 @@ const Home = ({
   romanceMovies,
   documentaries,
 }: Props) => {
-
-  const { loading } = useAuth()
+  const { loading } = useAuth();
   const showModal = useRecoilValue(modalState);
-  
 
-  if(loading) {
-    return null
+  if (loading) {
+    return null;
   }
 
   return (
-    <div className="relative h-screen bg-gradient-to-b  lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b  lg:h-[140vh] ${
+        showModal && "!h-screen overflow-hidden"
+      }`}
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
-        <Banner netflixOriginals={netflixOriginals}/>
+        <Banner netflixOriginals={netflixOriginals} />
         <section className="md:space-y-24">
           <Row title="Trending Now" movies={trendingNow} />
-          <Row title="Top Rated" movies={topRated}/>
-          <Row title="Action Thrillers" movies={actionMovies}/>
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
           {/* MyList */}
-          
-          <Row title="Comedies" movies={comedyMovies}/>
-          <Row title="Scary Movies" movies={horrorMovies}/>
-          <Row title="Romance Movies" movies={romanceMovies}/>
-          <Row title="Documentaries" movies={documentaries}/>
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-      {showModal && <Modal /> }
+      {showModal && <Modal />}
     </div>
   );
 };
